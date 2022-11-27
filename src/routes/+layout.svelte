ac4d1195840f3198c46f5f8862c2425412f88369
Khaled FERJANI <kferjani@linagora.com>
2022-11-27 17:02:39 +0100

    fixed weird background color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
 
-<div class="hero min-h-screen bg-base-200">
-  <div class="hero-content text-center">
-    <div class="max-w-2xl">
-      <slot />
-    </div>
-  </div>
+<div class="hero min-h-screen">
+	<div class="hero-content text-center">
+		<div class="max-w-2xl">
+			<slot />
+		</div>
+	</div>
 </div>
