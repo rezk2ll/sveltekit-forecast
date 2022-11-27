5d2e6a3e9cb8e16709ba08987806d47fc53a8ced
Khaled FERJANI <kferjani@linagora.com>
2022-11-27 15:52:28 +0100

    get units in metric

--- src/routes/weather/[long]/[lat]/+server.ts
+++ src/routes/weather/[long]/[lat]/+server.ts
@@ -1,16 +1,16 @@
 import type { RequestHandler } from './$types';
 import { WEATHER_API_KEY } from '$env/static/private';
-import { error } from "@sveltejs/kit";
+import { error } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async ({ params, fetch }) => {
-  const { long, lat } = params;
+	const { long, lat } = params;
 
-  	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${WEATHER_API_KEY}`;
-		try {
-			const response = await fetch(url);
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${WEATHER_API_KEY}`;
+	try {
+		const response = await fetch(url);
 
-      return response;
-		} catch (err) {
-			throw error(500, 'failed to fetch weather');
-		}
-}
+		return response;
+	} catch (err) {
+		throw error(500, 'failed to fetch weather');
+	}
+};
